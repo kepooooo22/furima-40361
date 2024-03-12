694c8838753a4ed00c067db66b4c05f6d7cddc60
kepooooo22 <kkk0220131kkk@ezweb.ne.jp>
2024-03-12 23:41:33 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
   # before_action :set_item, only: [:show, :edit, :update, :destroy]
+  def index
+    @items = Item.includes(:user).order('created_at DESC')
+  end
 
   def new
     @item = Item.new
