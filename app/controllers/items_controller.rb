59f021456f128f2423591c97d8a0e583fea296f5
kepooooo22 <kkk0220131kkk@ezweb.ne.jp>
2024-03-14 15:01:49 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -35,6 +35,11 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    @item.destroy if @item.user_id == current_user.id
+    redirect_to root_path
   end
 
   private
